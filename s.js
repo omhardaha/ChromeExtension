959cd9a895b68d17a863bca4b119d83fa85b2878
omhardaha <omprakashhardaha369@gmail.com>
2022-01-21 13:23:00 +0530

    full screen shot also done

--- s.js
+++ s.js
@@ -4,7 +4,7 @@ let urlForShor;
 // PART - 1 GENERATING QR CODE 🧨
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 
-    let url = `https://api.qrserver.com/v1/create-qr-code/?data=${tabs[0].url}&amp;size=50x50`;
+    let url = `https://api.qrserver.com/v1/create-qr-code/?data=${tabs[0].url}&amp;size=100x100`;
     urlForShor = tabs[0].url
     document.getElementById("qr").src = url;
 })
@@ -12,11 +12,19 @@ chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 // PART - 2 LINK SHORTNING🧨
 var link = document.getElementById('linkshort');
 link.addEventListener('click',async function(){
-    console.log( encodeURIComponent(urlForShor) );
-    const data = await fetch(` https://tinyurl.com/api-create.php?url=${ urlForShor }`);
 
-    let finalData = await data.text();
-    await console.log(finalData);
-    await navigator.clipboard.writeText(finalData);
+    const data = await (await fetch(` https://tinyurl.com/api-create.php?url=${ urlForShor }`)).text();
+
+    await navigator.clipboard.writeText(data);
+});
+
+// PART - 3 FULL SCREEN SCREENSHOT🧨
+
+var link = document.getElementById('ScreenShot');
+link.addEventListener('click',async function(){
+console.log("clicked");
+   await chrome.downloads.download({ url: `https://screenshot.abstractapi.com/v1/?api_key=95533e4b0c394bb0b3b2ee7032e3f03c&url=${ urlForShor }`, filename: "fullScreenshot.png", saveAs: true });
+
     console.log("hi data");
 });
+
