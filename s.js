815039f366911455f105183fd4cced7d32dbfce6
omhardaha <omprakashhardaha369@gmail.com>
2022-02-25 00:13:44 +0530

    completed this extenstion

--- s.js
+++ s.js
@@ -2,9 +2,10 @@
 let urlForShor;
 
 // PART - 1 GENERATING QR CODE 🧨
+ let url 
 chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
 
-    let url = `https://api.qrserver.com/v1/create-qr-code/?data=${tabs[0].url}&amp;size=100x100`;
+    url = `https://api.qrserver.com/v1/create-qr-code/?data=${tabs[0].url}&amp;size=100x100`;
     urlForShor = tabs[0].url
     document.getElementById("qr").src = url;
 })
@@ -16,7 +17,7 @@ link.addEventListener('click', async function () {
     const data = await (await fetch(` https://tinyurl.com/api-create.php?url=${urlForShor}`)).text();
 
     await navigator.clipboard.writeText(data);
-     link.innerText = await"Copied To ClipBoard";
+    link.innerText = await "Copied To ClipBoard";
 });
 
 // PART - 3 FULL SCREEN SCREENSHOT🧨
@@ -28,8 +29,20 @@ screenButton.addEventListener('click', async function () {
 
     await chrome.downloads.download({ url: `https://screenshot.abstractapi.com/v1/?api_key=95533e4b0c394bb0b3b2ee7032e3f03c&url=${urlForShor}`, filename: "fullScreenshot.png", saveAs: true });
 
-    screenButton.innerText = await"Saving";
+    screenButton.innerText = await "Saving";
 
     console.log("hi data");
 });
 
+//part 4 saving qr code
+let qrsave = document.getElementById('QRbutton1');
+qrsave.addEventListener('click', async function () {
+    // console.log("clicked");
+    // qrsave.innerText = "saving.."
+
+    // await chrome.downloads.download({ url: `https://api.qrserver.com/v1/create-qr-code/?data=${tabs[0].url}&amp;size=100x100`, filename: "QRCode.png", saveAs: true });
+
+    // qrsave.innerText = await "Saving";
+    window.open(url, '_blank');
+    console.log("hi data");
+});
